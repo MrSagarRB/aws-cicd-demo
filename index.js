3eee457bfb56993a789d2f8aa90b48e254cd090b
Sagar Borude <87903581+MrSagarRB@users.noreply.github.com>
2022-12-18 19:33:39 +0530

    home route

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
   res.send(countriesData);
 });
 
+app.get("/home", (req, res) => {
+  res.send("home-page");
+});
+
 app.listen(3005, () => {
   console.log("Server Running on Port 3005");
 });
